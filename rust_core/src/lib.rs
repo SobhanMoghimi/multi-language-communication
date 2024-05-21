960c41a7e7049249eb6337dd1fc0d6871868cf17
Sobhan Moghimi <sobhanmoghimi45@gmail.com>
2024-05-22 03:03:51 +0330

    Add semaphor and mutex to lib.rs

--- rust_core/src/lib.rs
+++ rust_core/src/lib.rs
@@ -1,8 +1,13 @@
-use libc::{c_char, c_int, mmap, munmap, shm_open, shm_unlink, ftruncate, O_CREAT, O_RDWR, PROT_READ, PROT_WRITE, MAP_SHARED, MAP_FAILED};
+use libc::{c_char, c_int, mmap, munmap, shm_open, shm_unlink, ftruncate,
+     O_CREAT, O_RDWR, PROT_READ, PROT_WRITE, MAP_SHARED, MAP_FAILED,
+      pthread_mutex_t, pthread_mutexattr_t, pthread_mutex_init, pthread_mutexattr_init,
+       pthread_mutexattr_setpshared, pthread_mutex_lock, pthread_mutex_unlock,
+        PTHREAD_PROCESS_SHARED, sem_t, sem_init, sem_destroy, sem_wait, sem_post};
 use std::ffi::{CStr, CString};
 use std::ptr;
 use std::slice;
 use std::str;
+use std::mem;
 
 // Define constants
 const SHM_NAME: &str = "/my_shared_memory";
@@ -17,29 +22,63 @@ struct QueueEntry {
     message: [u8; ENTRY_SIZE+1],  // Message as a fixed-size array
 }
 
-// Function to create shared memory
+#[repr(C)]
+struct SharedMemory {
+    mutex: pthread_mutex_t,
+    semaphore: sem_t,
+    input_queue: [QueueEntry; MAX_ENTRIES],
+    output_queue: [QueueEntry; MAX_ENTRIES],
+}
+
+impl Default for SharedMemory {
+    fn default() -> Self {
+        unsafe {
+            let mut shared_memory: SharedMemory = mem::zeroed();
+
+            let mut attr: pthread_mutexattr_t = mem::zeroed();
+            pthread_mutexattr_init(&mut attr);
+            pthread_mutexattr_setpshared(&mut attr, PTHREAD_PROCESS_SHARED);
+            pthread_mutex_init(&mut shared_memory.mutex, &attr);
+
+            sem_init(&mut shared_memory.semaphore, 1, 1);
+
+            shared_memory
+        }
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn create_shared_memory() -> c_int {
     unsafe {
-        let shm_fd = shm_open(CString::new(SHM_NAME).unwrap().as_ptr(), O_CREAT | O_RDWR, 0o666);
+        let shm_name = CString::new(SHM_NAME).unwrap();
+        let shm_fd = shm_open(shm_name.as_ptr(), O_CREAT | O_RDWR, 0o666);
         if shm_fd == -1 {
             eprintln!("Failed to open shared memory");
             return -1;
         }
 
-        if ftruncate(shm_fd, SHM_SIZE as i64) == -1 {
+        if ftruncate(shm_fd, mem::size_of::<SharedMemory>() as i64) == -1 {
             eprintln!("Failed to truncate shared memory");
-            shm_unlink(CString::new(SHM_NAME).unwrap().as_ptr());
+            shm_unlink(shm_name.as_ptr());
             return -1;
         }
 
+        let ptr = mmap(ptr::null_mut(), mem::size_of::<SharedMemory>(), PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        if ptr == MAP_FAILED {
+            eprintln!("Failed to map shared memory");
+            return -1;
+        }
+
+        let shared_memory: &mut SharedMemory = &mut *(ptr as *mut SharedMemory);
+        *shared_memory = SharedMemory::default();
+
+        munmap(ptr, mem::size_of::<SharedMemory>());
         shm_fd
     }
 }
 
 // Function to write to a queue in shared memory
-unsafe fn write_to_queue(ptr: *mut u8, uuid: *const c_char, data: *const c_char) -> c_int {
-    let queue: &mut [QueueEntry] = slice::from_raw_parts_mut(ptr as *mut QueueEntry, MAX_ENTRIES);
+unsafe fn write_to_queue(queue: &mut [QueueEntry], uuid: *const c_char, data: *const c_char) -> c_int {
     let uuid_str = CStr::from_ptr(uuid).to_str().unwrap();
     let data_str = CStr::from_ptr(data).to_str().unwrap();
 
@@ -48,15 +87,15 @@ unsafe fn write_to_queue(ptr: *mut u8, uuid: *const c_char, data: *const c_char)
             // Clear the entry before copying
             entry.uuid = [0; 36+1];
             entry.message = [0; ENTRY_SIZE+1];
-            
+
             // Copy the UUID and message, including null terminators
             ptr::copy_nonoverlapping(uuid, entry.uuid.as_mut_ptr() as *mut c_char, uuid_str.len());
             entry.uuid[uuid_str.len()] = 0; // null-terminate
-            
+
             ptr::copy_nonoverlapping(data, entry.message.as_mut_ptr() as *mut c_char, data_str.len());
             entry.message[data_str.len()] = 0; // null-terminate
-            
-            // println!("Write: UUID={} Message={}", uuid_str, data_str);
+
+            println!("Write: UUID={} Message={}", uuid_str, data_str);
             break;
         }
     }
@@ -64,13 +103,12 @@ unsafe fn write_to_queue(ptr: *mut u8, uuid: *const c_char, data: *const c_char)
 }
 
 // Function to read from a queue in shared memory
-unsafe fn read_from_queue(ptr: *const u8) -> *mut c_char {
-    let queue: &[QueueEntry] = slice::from_raw_parts(ptr as *const QueueEntry, MAX_ENTRIES);
+unsafe fn read_from_queue(queue: &[QueueEntry]) -> *mut c_char {
     let entry = queue.iter().find(|entry| entry.uuid[0] != 0);
 
     if let Some(entry) = entry {
         let message = CStr::from_bytes_with_nul_unchecked(&entry.message);
-        // println!("Read: UUID={} Message={}", String::from_utf8_lossy(&entry.uuid), message.to_str().unwrap_or(""));
+        println!("Read: UUID={} Message={}", String::from_utf8_lossy(&entry.uuid), message.to_str().unwrap_or(""));
         return CString::new(message.to_bytes()).unwrap_unchecked().into_raw();
     } else {
         return ptr::null_mut();
@@ -78,45 +116,42 @@ unsafe fn read_from_queue(ptr: *const u8) -> *mut c_char {
 }
 
 // Function to remove from a queue in shared memory
-// unsafe fn remove_from_queue(ptr: *mut u8, uuid: *const c_char) -> c_int {
-//     let queue: &mut [QueueEntry] = slice::from_raw_parts_mut(ptr as *mut QueueEntry, MAX_ENTRIES);
-//     let uuid_str = CStr::from_ptr(uuid).to_str().unwrap();
-
-//     for entry in queue.iter_mut() {
-//         if entry.uuid.starts_with(uuid_str.as_bytes()) {
-//             entry.uuid = [0; 36];
-//             entry.message = [0; ENTRY_SIZE];
-//             break;
-//         }
-//     }
-//     0
-// }
-
-// Function to remove from a queue in shared memory
-unsafe fn remove_from_queue(ptr: *mut u8, uuid: *const c_char) -> c_int {
-    let queue: &mut [QueueEntry] = slice::from_raw_parts_mut(ptr as *mut QueueEntry, MAX_ENTRIES);
+unsafe fn remove_from_queue(queue: &mut [QueueEntry], uuid: *const c_char) -> c_int {
     let uuid_str = CStr::from_ptr(uuid).to_str().unwrap();
 
     for entry in queue.iter_mut() {
         if entry.uuid.starts_with(uuid_str.as_bytes()) {
             entry.uuid = [0; 36+1];
             entry.message = [0; ENTRY_SIZE+1];
-            // println!("Removed: UUID={}", uuid_str);
-            break;
+            println!("Removed: UUID={}", uuid_str);
+            return 0; // Indicate success
         }
     }
-    0
+
+    -1 // Indicate failure (UUID not found)
 }
+
 
 #[no_mangle]
 pub extern "C" fn write_to_input_queue(shm_fd: c_int, uuid: *const c_char, data: *const c_char) -> c_int {
     unsafe {
-        let ptr = mmap(ptr::null_mut(), SHM_SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        let ptr = mmap(ptr::null_mut(), mem::size_of::<SharedMemory>(), PROT_WRITE, MAP_SHARED, shm_fd, 0);
         if ptr == MAP_FAILED {
+            eprintln!("Failed to map shared memory");
             return -1;
         }
-        let result = write_to_queue(ptr as *mut u8, uuid, data);
-        munmap(ptr, SHM_SIZE);
+
+        let shared_memory: &mut SharedMemory = &mut *(ptr as *mut SharedMemory);
+
+        pthread_mutex_lock(&mut shared_memory.mutex);
+        sem_wait(&mut shared_memory.semaphore);
+
+        let result = write_to_queue(&mut shared_memory.input_queue, uuid, data);
+
+        sem_post(&mut shared_memory.semaphore);
+        pthread_mutex_unlock(&mut shared_memory.mutex);
+
+        munmap(ptr, mem::size_of::<SharedMemory>());
         result
     }
 }
@@ -124,95 +159,160 @@ pub extern "C" fn write_to_input_queue(shm_fd: c_int, uuid: *const c_char, data:
 #[no_mangle]
 pub extern "C" fn read_from_input_queue(shm_fd: c_int) -> *mut c_char {
     unsafe {
-        let ptr = mmap(ptr::null_mut(), SHM_SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+        let ptr = mmap(ptr::null_mut(), mem::size_of::<SharedMemory>(), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
         if ptr == MAP_FAILED {
+            eprintln!("Failed to map shared memory");
             return ptr::null_mut();
         }
-        let result = read_from_queue(ptr as *const u8);
-        munmap(ptr, SHM_SIZE);
-        return result;
-    }
-}
 
-// Function to remove message from input queue by UUID in shared memory
-#[no_mangle]
-pub extern "C" fn remove_from_input_queue(shm_fd: c_int, uuid: *const c_char) -> c_int {
-    unsafe {
-        let ptr = mmap(ptr::null_mut(), SHM_SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
-        if ptr == MAP_FAILED {
-            return -1;
-        }
+        let shared_memory: &SharedMemory = &*(ptr as *const SharedMemory);
 
-        let result = remove_from_queue(ptr as *mut u8, uuid);
-        munmap(ptr, SHM_SIZE);
+        // Lock the mutex
+        let mutex_ptr = &shared_memory.mutex as *const pthread_mutex_t as *mut pthread_mutex_t;
+        pthread_mutex_lock(mutex_ptr);
+
+        // Wait on the semaphore
+        let sem_ptr = &shared_memory.semaphore as *const sem_t as *mut sem_t;
+        sem_wait(sem_ptr);
+
+        // Read from the input queue
+        let result = read_from_queue(&shared_memory.input_queue);
+
+        // Post the semaphore
+        sem_post(sem_ptr);
+
+        // Unlock the mutex
+        pthread_mutex_unlock(mutex_ptr);
+
+        munmap(ptr, mem::size_of::<SharedMemory>());
         result
     }
 }
+
+
+// Function to remove message from input queue by UUID in shared memory
+#[no_mangle]
+pub extern "C" fn read_from_output_queue(shm_fd: c_int) -> *mut c_char {
+    unsafe {
+        let ptr = mmap(ptr::null_mut(), mem::size_of::<SharedMemory>(), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        if ptr == MAP_FAILED {
+            eprintln!("Failed to map shared memory");
+            return ptr::null_mut();
+        }
+
+        let shared_memory: &SharedMemory = &*(ptr as *const SharedMemory);
+
+        // Lock the mutex
+        let mutex_ptr = &shared_memory.mutex as *const pthread_mutex_t as *mut pthread_mutex_t;
+        pthread_mutex_lock(mutex_ptr);
+
+        // Wait on the semaphore
+        let sem_ptr = &shared_memory.semaphore as *const sem_t as *mut sem_t;
+        sem_wait(sem_ptr);
+
+        // Read from the output queue
+        let result = read_from_queue(&shared_memory.output_queue);
+
+        // Post the semaphore
+        sem_post(sem_ptr);
+
+        // Unlock the mutex
+        pthread_mutex_unlock(mutex_ptr);
+
+        munmap(ptr, mem::size_of::<SharedMemory>());
+        result
+    }
+}
+
+
 // Function to write to output queue in shared memory
 #[no_mangle]
 pub extern "C" fn write_to_output_queue(shm_fd: c_int, uuid: *const c_char, data: *const c_char) -> c_int {
     unsafe {
-        let ptr = mmap(ptr::null_mut(), SHM_SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        let ptr = mmap(ptr::null_mut(), mem::size_of::<SharedMemory>(), PROT_WRITE, MAP_SHARED, shm_fd, 0);
         if ptr == MAP_FAILED {
+            eprintln!("Failed to map shared memory");
             return -1;
         }
 
+        let shared_memory: &mut SharedMemory = &mut *(ptr as *mut SharedMemory);
+
+        pthread_mutex_lock(&mut shared_memory.mutex);
+        sem_wait(&mut shared_memory.semaphore);
+
         // Write to the second half of the shared memory (output queue)
-        let result = write_to_queue(ptr.add(SHM_SIZE / 2) as *mut u8, uuid, data);
-        munmap(ptr, SHM_SIZE);
+        let result = write_to_queue(&mut shared_memory.output_queue, uuid, data);
+
+        sem_post(&mut shared_memory.semaphore);
+        pthread_mutex_unlock(&mut shared_memory.mutex);
+
+        munmap(ptr, mem::size_of::<SharedMemory>());
         result
     }
 }
 
-// Function to read from output queue in shared memory
-#[no_mangle]
-pub extern "C" fn read_from_output_queue(shm_fd: c_int) -> *mut c_char {
-    unsafe {
-        let ptr = mmap(ptr::null_mut(), SHM_SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
-        if ptr == MAP_FAILED {
-            return ptr::null_mut();
-        }
-
-        // Read from the second half of the shared memory (output queue)
-        let result = read_from_queue(ptr.add(SHM_SIZE / 2) as *const u8);
-        munmap(ptr, SHM_SIZE);
-        result
-    }
-}
-
-// Function to remove message from output queue by UUID in shared memory
 #[no_mangle]
 pub extern "C" fn remove_from_output_queue(shm_fd: c_int, uuid: *const c_char) -> c_int {
     unsafe {
-        let ptr = mmap(ptr::null_mut(), SHM_SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        let ptr = mmap(ptr::null_mut(), mem::size_of::<SharedMemory>(), PROT_WRITE, MAP_SHARED, shm_fd, 0);
         if ptr == MAP_FAILED {
+            eprintln!("Failed to map shared memory");
             return -1;
         }
 
+        let shared_memory: &mut SharedMemory = &mut *(ptr as *mut SharedMemory);
+
+        pthread_mutex_lock(&mut shared_memory.mutex);
+        sem_wait(&mut shared_memory.semaphore);
+
         // Remove from the second half of the shared memory (output queue)
-        let result = remove_from_queue(ptr.add(SHM_SIZE / 2) as *mut u8, uuid);
-        munmap(ptr, SHM_SIZE);
+        let result = remove_from_queue(&mut shared_memory.output_queue, uuid);
+
+        sem_post(&mut shared_memory.semaphore);
+        pthread_mutex_unlock(&mut shared_memory.mutex);
+
+        munmap(ptr, mem::size_of::<SharedMemory>());
         result
     }
 }
-// Function to clear shared memory (clear both input and output queues)
+
 #[no_mangle]
 pub extern "C" fn clear_shared_memory(shm_fd: c_int) -> c_int {
     unsafe {
-        let ptr = mmap(ptr::null_mut(), SHM_SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        println!("Mapping shared memory with size: {}", mem::size_of::<SharedMemory>());
+        let ptr = mmap(ptr::null_mut(), mem::size_of::<SharedMemory>(), PROT_WRITE, MAP_SHARED, shm_fd, 0);
         if ptr == MAP_FAILED {
+            eprintln!("Failed to map shared memory");
             return -1;
         }
 
-        let input_queue: &mut [QueueEntry] = slice::from_raw_parts_mut(ptr as *mut QueueEntry, MAX_ENTRIES);
-        let output_queue: &mut [QueueEntry] = slice::from_raw_parts_mut(ptr.add(SHM_SIZE / 2) as *mut QueueEntry, MAX_ENTRIES);
+        let shared_memory: &mut SharedMemory = &mut *(ptr as *mut SharedMemory);
 
-        for entry in input_queue.iter_mut().chain(output_queue.iter_mut()) {
+        pthread_mutex_lock(&mut shared_memory.mutex);
+        sem_wait(&mut shared_memory.semaphore);
+
+        println!("Clearing input queue");
+        for entry in shared_memory.input_queue.iter_mut() {
             entry.uuid = [0; 36+1];
             entry.message = [0; ENTRY_SIZE+1];
         }
 
-        munmap(ptr, SHM_SIZE);
+        println!("Clearing output queue");
+        for entry in shared_memory.output_queue.iter_mut() {
+            entry.uuid = [0; 36+1];
+            entry.message = [0; ENTRY_SIZE+1];
+        }
+
+        sem_post(&mut shared_memory.semaphore);
+        pthread_mutex_unlock(&mut shared_memory.mutex);
+
+        println!("Unmapping shared memory");
+        if munmap(ptr, mem::size_of::<SharedMemory>()) == -1 {
+            eprintln!("Failed to unmap shared memory");
+            return -1;
+        }
+
+        println!("Shared memory unmapped successfully");
         0
     }
 }
--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from time import sleep
 
 
 logging.basicConfig()
-logging.getLogger().setLevel(logging.ERROR)
+logging.getLogger().setLevel(logging.INFO)
 
 ffi = cffi.FFI()
 
@@ -43,15 +43,16 @@ def call_function(call_function_args):
         "args": args
     }
     C.write_to_input_queue(shm_fd, call_uuid.encode('utf-8'), json.dumps(function_call).encode('utf-8'))
-   
+    print("READING")
     message_ptr = C.read_from_input_queue(shm_fd)
+    print("READ")
     # print(message_ptr)
     if message_ptr:
         message = ffi.string(message_ptr).decode('utf-8')
         logging.info(f"Code add.py recieved {message}!")    # Run the corresponding function program
         logging.info(f"message is : {message}")
 
-    subprocess.call([function_call_command, function_location])
+    # subprocess.call([function_call_command, function_location])
 
     # Wait for result
     result = None
@@ -73,34 +74,35 @@ if __name__ == "__main__":
         result = call_function(["python3", "add.py", "add", {"a": 10, "b": 4}])
         print(f"Add Result: {result}")
 
-        result = call_function(["python3", "add.py", "add", {"a": 12, "b": 5}])
-        print(f"Add Result: {result}")
+        # result = call_function(["python3", "add.py", "add", {"a": 12, "b": 5}])
+        # print(f"Add Result: {result}")
 
-        result = call_function(["python3", "add.py", "add", {"a": 9, "b": 13}])
-        print(f"Add Result: {result}")
+        # result = call_function(["python3", "add.py", "add", {"a": 9, "b": 13}])
+        # print(f"Add Result: {result}")
 
 
-        # multiprocessing pool object 
-        pool = multiprocessing.Pool() 
+        # # multiprocessing pool object 
+        # pool = multiprocessing.Pool() 
     
-        # pool object with number of element 
-        pool = multiprocessing.Pool(processes=4) 
+        # # pool object with number of element 
+        # pool = multiprocessing.Pool(processes=3) 
     
-        # input list 
-        inputs = [["python3", "add.py", "add", {"a": 1234, "b": 0}],
-                  ["python3", "add.py", "add", {"a": 2, "b": 123}],
-                  ["python3", "add.py", "add", {"a": 10, "b": 75}]] 
+        # # input list 
+        # inputs = [["python3", "add.py", "add", {"a": 1234, "b": 0}],
+        #           ["python3", "add.py", "add", {"a": 2, "b": 123}],
+        #           ["python3", "add.py", "add", {"a": 10, "b": 75}]] 
     
-        # map the function to the list and pass 
-        # function and input list as arguments 
-        outputs = pool.map(call_function, inputs) 
+        # # map the function to the list and pass 
+        # # function and input list as arguments 
+        # outputs = pool.map(call_function, inputs) 
     
-        # Print input list 
-        print("Input: {}".format(inputs)) 
+        # # Print input list 
+        # print("Input: {}".format(inputs)) 
     
-        # Print output list 
-        print("Output: {}".format(outputs)) 
+        # # Print output list 
+        # print("Output: {}".format(outputs)) 
 
+        # C.clear_shared_memory(shm_fd)
     except Exception as e:
         print(e)
 
